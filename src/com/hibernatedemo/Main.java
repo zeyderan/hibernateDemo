698bb9d0027f4ac5e5f736386ef2dea1887353eb
zeyderan <aykutbayraktar34@gmail.com>
2020-07-26 11:48:35 +0300

    crud işlemleri incelendi

--- src/com/hibernatedemo/Main.java
+++ src/com/hibernatedemo/Main.java
@@ -31,14 +31,39 @@ public class Main {
 //			burada sorgu gönderilip sonuç list içerisine alýnýr
 //			city deðil City yazýlmalý class ismi
 //			from City c where c.countryCode='TUR' AND c.district='Istanbul bölgesi istanbul olan türkiyedeli þehirleri getir
-			List<String> countryCodes = session.createQuery("select c.countryCode from City c GROUP BY c.countryCode").getResultList();
+			/*
+			 * List<String> countryCodes =
+			 * session.createQuery("select c.countryCode from City c GROUP BY c.countryCode"
+			 * ).getResultList();
+			 * 
+			 * // list elemanlarýný ekrana yazdýrýr for(String countryCode:countryCodes) {
+			 * // tablodaki name sütunu içerisindekiler okur
+			 * System.out.println(countryCode); }
+			 */
 			
-//			list elemanlarýný ekrana yazdýrýr
-			for(String countryCode:countryCodes) {
-//				tablodaki name sütunu içerisindekiler okur
-				System.out.println(countryCode);
-			}
-			session.getTransaction().commit();
+			//insert
+//			City city = new City();
+//			city.setName("Düzce 18");
+//			city.setCountryCode("TUR");
+//			city.setDistrict("Istanbul");
+//			city.setPopulation(10000);
+//			session.save(city);
+			
+			//update
+//			City city = session.get(City.class,4080);
+//			
+//			city.setPopulation(20000);
+//			
+//			session.save(city);
+//			
+//			session.getTransaction().commit();
+			
+			//delete
+			
+			City city = session.get(City.class, 4080);
+			session.delete(city);
+			//save e burada gerek yoktur.
+
 		}finally {
 //			session factroy i kapatýr
 			factory.close();
