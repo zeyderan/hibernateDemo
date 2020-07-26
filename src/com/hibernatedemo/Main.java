736a53b7061f4845c62b414e5d12240fbb208935
zeyderan <aykutbayraktar34@gmail.com>
2020-07-26 10:23:48 +0300

    açıklamalar ekledi

--- src/com/hibernatedemo/Main.java
+++ src/com/hibernatedemo/Main.java
@@ -1,6 +1,5 @@
 package com.hibernatedemo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -11,23 +10,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+//		hibernate ile veritabanýna session gönderilir.
+//		session içerisinde sorgularý barýndýrýr.
+//		session transaction iþlemleri için gereklidir
+//		bu nedenle önce session factory ile session oluþturacak fatory elde edilir
 		SessionFactory factory = new Configuration()
+//				factory hangi ayarlarý kullaacak 
 				.configure("hibernate.cfg.xml")
+//				hangi pojo kulanýlacak
 				.addAnnotatedClass(City.class)
 				.buildSessionFactory();
 		
+//		session nesnesini alýnýr
 		Session session = factory.getCurrentSession();
 		
 		try {
+			
 			session.beginTransaction();
 			
+//			burada sorgu gönderilip sonuç list içerisine alýnýr
 			List<City> cities = session.createQuery("from City").getResultList();
 			
+//			list elemanlarýný ekrana yazdýrýr
 			for(City city:cities) {
+//				tablodaki name sütunu içerisindekiler okur
 				System.out.println(city.getName());
 			}
 			session.getTransaction().commit();
 		}finally {
+//			session factroy i kapatýr
 			factory.close();
 		}
 	}
